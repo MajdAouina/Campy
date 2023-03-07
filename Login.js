164978225f8d681d478f68913f159cb265784c84
mohamed aziz guerfala <guerfalamohamed2001@gmail.com>
2023-03-07 14:22:29 +0100

    test

--- Login.js
+++ Login.js
@@ -1,4 +1,4 @@
-console.log('nam')
+
 // sign up function 
 function signup(){
 // e.preventDefault();
@@ -19,5 +19,58 @@ localStorage.setItem(username,json )
 console.log("user add")
 
 }
+
+function login(){
+    var email1=document.getElementsByClassName('email').value;
+    var pass1 =document.getElementsByClassName('pass').value;
+    
+    var email=localStorage.getItem("email");
+    var pass=localStorage.getItem("pass");
+    console.log(email,pass)
+
+
+    if(email1===email &&  pass1 ===pass){
+        // return "welcome back " + this.Account 
+       alert("welcome to campy")  
+        window.location.href="home.html"
+   
+    }
+
+    else {
+       
+    alert("your email or password is incorrect")
+    }
+
+
+
+    // var userExists = false;
+    // for (var i = 0; i < localStorage.length; i++) {
+    //   var key = localStorage.key(i);
+    // //   var   = JSON.parse(localStorage.getItem(key));
+    //   if (email === email1 && pass === pass1) {
+    //     userExists = true;
+    //     window.location.href="home.html"
+    //     break;
+    //   }
+    // }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
